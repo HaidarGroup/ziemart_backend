cdbb527e9d5e9b48259b954e59cce1af97bebe8b
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-10-04 21:44:08 +0700

    nambah register-login,lupapassord, vreifikasi

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerController extends Controller
 {
-    //
+        public function index(){
+        $data = Buyer::all();
+        return response()->json($data);
+    }
 }
